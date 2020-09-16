dac6e86e6d236be3a911fb5ccba8796906514e67
Jeff Mark <net@jeffmark.com>
2020-09-16 07:20:44 -0700

    - update counter to show "soon" at the end - add music index to output at start

--- counter.php
+++ counter.php
@@ -5,44 +5,70 @@
 
     function output($min, $sec) {
         $str = "";
-        if ($min > 99) {
-            $hour = intval( $min / 60 );
-            $str = $hour . ":";
-            $min = $min % 60;
+        if ($min >= 0 || $sec >= 0) {
+            if ($min > 99) {
+                $hour = intval( $min / 60 );
+                $str = $hour . ":";
+                $min = $min % 60;
+            }
+            $min = str_pad($min, 2, "0", STR_PAD_LEFT);
+            $sec = str_pad($sec, 2, "0", STR_PAD_LEFT);
+            $str .= $min . ":" . $sec;
+        } else {
+            $str = "SOON!";
         }
-        $min = str_pad($min, 2, "0", STR_PAD_LEFT);
-        $sec = str_pad($sec, 2, "0", STR_PAD_LEFT);
-        $str .= $min . ":" . $sec;
         print $str . "\n";
         file_put_contents("counter.txt", $str);
     }
 
-    if ($argc <= 1) {
+    function usage() {
+        global $argv;
+
         $now = new DateTime();
         $formatted = $now->format('Y-m-d\TH:i:sO');
-        echo "\n\n";
+        echo "\n";
         echo "USAGE: php " . $argv[0] . " " . $formatted;
         echo "\n\n";
+    }
+
+    if ($argc <= 1) {
+        usage();
         exit;
     }
 
     $jsDeadline = $argv[1];
-    if (isset($jsDeadline)) {
-        $startTime = new DateTime($jsDeadline);
-        $liveWorshipDate = $startTime->format('D M j @ g:ia');
-
-        $deadline = new DateTime($jsDeadline);
-        $deadlineRealTS = $deadline->getTimestamp();
-
-        $diffReal = $deadlineRealTS - time();
-        while ($diffReal > 0) {
-            $min = intval( $diffReal / 60 );
-            $sec = $diffReal % 60;
-            output($min, $sec);
-            sleep(1);
-            $diffReal = $deadlineRealTS - time();
-        }
-
-        output(0, 0);
+    if (!isset($jsDeadline)) {
+        usage();
+        exit;
     }
+
+    $startTime = new DateTime($jsDeadline);
+
+    $MUSIC_INDEX_FILES = 8;
+
+    echo "\n\n";
+    echo "Current Index:\n";
+    $dayOfYear = $startTime->format('z');
+    $musicIndex = ($dayOfYear % $MUSIC_INDEX_FILES) + 1;
+    echo $musicIndex;
+    echo "\n\n";
+
+    $liveWorshipDate = $startTime->format('D M j @ g:ia');
+    echo $liveWorshipDate;
+    echo "\n\n";
+
+    $deadlineRealTS = $startTime->getTimestamp();
+
+    $diffReal = $deadlineRealTS - time();
+    while ($diffReal > 0) {
+        $min = intval( $diffReal / 60 );
+        $sec = $diffReal % 60;
+        output($min, $sec);
+        sleep(1);
+        $diffReal = $deadlineRealTS - time();
+    }
+
+    output(0, 0);
+    sleep(5);
+    output(-1, -1);
 ?>
